f327871095295f9c7fed9321bf2034b9f102a5e0
simon <simonakuliesho@gmail.com>
2022-11-06 17:15:36 +0200

    Add locators to Cookies/Eavalyne/Locators class

--- src/test/java/eavalyne/pages/Locators.java
+++ src/test/java/eavalyne/pages/Locators.java
@@ -1,4 +1,12 @@
 package eavalyne.pages;
 
+import org.openqa.selenium.By;
+
 public class Locators {
+    public static class Eavalyne{
+        public static class Cookies{
+            public static By cookiesBlog = By.xpath("//*[@id='marketing-approvals']");
+            public static By cookiesAccept = By.xpath("//*[@class = 'accept-button base-button primary normal green']");
+        }
+    }
 }
