2ccefcf7046a898c287fc34a242142e9149a7090
simon <simonakuliesho@gmail.com>
2022-11-06 16:34:14 +0200

    Add open method to LoginPage class

--- src/test/java/eavalyne/pages/eavalyneLt/LoginPage.java
+++ src/test/java/eavalyne/pages/eavalyneLt/LoginPage.java
@@ -1,4 +1,11 @@
 package eavalyne.pages.eavalyneLt;
 
+import eavalyne.pages.Common;
+
 public class LoginPage {
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
+
+   // public static void acceptPrivacyConfirmation() {}
 }
