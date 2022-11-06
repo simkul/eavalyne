fbdc24523c661b02f9942d528af2bf720dc2c30c
simon <simonakuliesho@gmail.com>
2022-11-06 17:13:24 +0200

    Add acceptPrivacyCongirmation method to LoginPage class

--- src/test/java/eavalyne/pages/eavalyneLt/LoginPage.java
+++ src/test/java/eavalyne/pages/eavalyneLt/LoginPage.java
@@ -1,11 +1,15 @@
 package eavalyne.pages.eavalyneLt;
 
 import eavalyne.pages.Common;
+import eavalyne.pages.Locators;
 
 public class LoginPage {
     public static void open(String url) {
         Common.openUrl(url);
     }
 
-   // public static void acceptPrivacyConfirmation() {}
+    public static void acceptPrivacyConfirmation() {
+        Common.waitForElementWithVisabilityChange(Locators.Eavalyne.Cookies.cookiesBlog);
+        Common.clickElement(Locators.Eavalyne.Cookies.cookiesAccept);
+    }
 }
