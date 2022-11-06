b8905a461a329603356cea56e5ff4cbc6f8499b5
simon <simonakuliesho@gmail.com>
2022-11-06 15:52:02 +0200

    Add setUp method to TestBase

--- src/test/java/eavalyne/tests/TestBase.java
+++ src/test/java/eavalyne/tests/TestBase.java
@@ -1,4 +1,10 @@
 package eavalyne.tests;
 
+import eavalyne.pages.Common;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public class TestBase {
+    @BeforeMethod
+    public void setUp(){Common.setUpDriver();}
 }
