97214e14a6d09a58a319456f2c9c419384328431
simon <simonakuliesho@gmail.com>
2022-11-06 17:14:44 +0200

    Add wait,click and get Element method to Common class

--- src/test/java/eavalyne/pages/Common.java
+++ src/test/java/eavalyne/pages/Common.java
@@ -1,6 +1,12 @@
 package eavalyne.pages;
 
 import eavalyne.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Common {
     public static void setUpDriver() {Driver.setDriver();}
@@ -9,5 +15,18 @@ public class Common {
 
     public static void openUrl(String url) {
         Driver.getDriver().get(url);
+    }
+
+    public static void waitForElementWithVisabilityChange(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(8));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
     }
 }
