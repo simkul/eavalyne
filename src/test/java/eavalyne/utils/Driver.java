36766bcafc1b2aa25ddfb817a08e90d2881722ee
simon <simonakuliesho@gmail.com>
2022-11-06 15:55:58 +0200

    Add setDriver and setUpOptions methods to Driver class

--- src/test/java/eavalyne/utils/Driver.java
+++ src/test/java/eavalyne/utils/Driver.java
@@ -1,4 +1,24 @@
 package eavalyne.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Driver {
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+
+        driver.set(new ChromeDriver(setUpOptions()));
+        driver.get().manage().deleteAllCookies();
+    }
+
+    private static ChromeOptions setUpOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=2000,3000");
+        options.addArguments("--force-device-scale-factor=0.75");
+        return options;
+    }
 }
