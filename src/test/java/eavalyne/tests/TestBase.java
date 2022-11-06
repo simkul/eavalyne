4502c51083ad0c38017b1f8ae22f373c59e908d5
simon <simonakuliesho@gmail.com>
2022-11-06 16:04:35 +0200

    Add tearDown method to TestBase

--- src/test/java/eavalyne/tests/TestBase.java
+++ src/test/java/eavalyne/tests/TestBase.java
@@ -1,10 +1,16 @@
 package eavalyne.tests;
 
 import eavalyne.pages.Common;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+
 
 public class TestBase {
     @BeforeMethod
     public void setUp(){Common.setUpDriver();}
+
+    @AfterMethod
+    public void tearDown() {
+        Common.closeDriver();
+    }
 }
