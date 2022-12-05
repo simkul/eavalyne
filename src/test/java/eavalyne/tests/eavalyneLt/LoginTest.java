3ad464dcb85c75cd31c1d29c27e808f0bb3970dc
simon <simonakuliesho@gmail.com>
2022-12-05 13:36:24 +0200

    Add loginWithCorectData Test

--- src/test/java/eavalyne/tests/eavalyneLt/LoginTest.java
+++ src/test/java/eavalyne/tests/eavalyneLt/LoginTest.java
@@ -2,7 +2,9 @@ package eavalyne.tests.eavalyneLt;
 
 import eavalyne.pages.eavalyneLt.LoginPage;
 import eavalyne.tests.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
     @BeforeMethod
@@ -11,6 +13,29 @@ public class LoginTest extends TestBase {
         super.setUp();
         LoginPage.open("https://eavalyne.lt/");
         LoginPage.acceptPrivacyConfirmation();
+    }
+    @Test
+    private void loginWihtCorectData(){
+        String insertEmail = "apallinarija@gmail.com";
+        String insertPassword = "Test1234";
+        String expectedRezult = "Mano paskyra";
+        String actualRezult = null;
+
+        LoginPage.clickOnPrisijungtibutton();
+        LoginPage.waitForVisability();
+        LoginPage.enterEmail(insertEmail);
+        LoginPage.enterPassword(insertPassword);
+        LoginPage.clickOnSecondPrisijungtiButton();
+        LoginPage.waitForPageLoad();
+
+        actualRezult = LoginPage.readOpenedPage();
+
+        Assert.assertTrue(actualRezult.contains(expectedRezult),
+                String.format("Actual [%s]; Expected [%s]",
+                        actualRezult,
+                        expectedRezult)
+        );
+
     }
 
 }
