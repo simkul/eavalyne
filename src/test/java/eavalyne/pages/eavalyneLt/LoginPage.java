41e051c5dffcec983be4032389a7c180c32d8365
simon <simonakuliesho@gmail.com>
2022-12-05 13:37:14 +0200

    Add methods to LoginPage class

--- src/test/java/eavalyne/pages/eavalyneLt/LoginPage.java
+++ src/test/java/eavalyne/pages/eavalyneLt/LoginPage.java
@@ -12,4 +12,31 @@ public class LoginPage {
         Common.waitForElementWithVisabilityChange(Locators.Eavalyne.Cookies.cookiesBlog);
         Common.clickElement(Locators.Eavalyne.Cookies.cookiesAccept);
     }
+
+    public static void clickOnPrisijungtibutton() {
+        Common.clickElement(Locators.Eavalyne.Prisijungti.prisijungtiButton);
+    }
+
+    public static void waitForVisability() {
+        Common.waitForElementWithVisabilityChange(Locators.Eavalyne.Prisijungti.prisijungtiForm);
+    }
+
+    public static void enterEmail(String inserEmail) {
+        Common.sendKeysToElement(inserEmail,Locators.Eavalyne.Prisijungti.emailLine);
+    }
+
+    public static void enterPassword(String insertPassword) {
+        Common.sendKeysToElement(insertPassword,Locators.Eavalyne.Prisijungti.passwordLine);
+    }
+
+    public static void clickOnSecondPrisijungtiButton() {
+        Common.clickElement(Locators.Eavalyne.Prisijungti.secondPrisijungtiButton);
+    }
+
+    public static void waitForPageLoad() {
+        Common.waitForElementWithVisabilityChange(Locators.Eavalyne.Prisijungti.newPageLoad);
+    }
+
+    public static String readOpenedPage() {return Common.getElementText(Locators.Eavalyne.Prisijungti.paragraphName);
+    }
 }
