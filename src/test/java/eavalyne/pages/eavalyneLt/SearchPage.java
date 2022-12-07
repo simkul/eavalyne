5c289be7bca60550f9a86f217ccef3400de22ced
simon <simonakuliesho@gmail.com>
2022-12-07 11:34:07 +0200

    Add enterItemBrandName method to SearcPage class

--- src/test/java/eavalyne/pages/eavalyneLt/SearchPage.java
+++ src/test/java/eavalyne/pages/eavalyneLt/SearchPage.java
@@ -1,4 +1,10 @@
 package eavalyne.pages.eavalyneLt;
 
+import eavalyne.pages.Common;
+import eavalyne.pages.Locators;
+
 public class SearchPage {
+    public static void enterItemBrandName(String insertBrandName) {
+        Common.sendKeysToElement(insertBrandName,Locators.Eavalyne.Search.searchLine);
+    }
 }
