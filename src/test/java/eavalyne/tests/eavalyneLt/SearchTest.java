1d7e0c708fd2f37dc1dd267a8e4a8b27261f223a
simon <simonakuliesho@gmail.com>
2022-12-07 11:21:39 +0200

    Add Before method to SearchTest

--- src/test/java/eavalyne/tests/eavalyneLt/SearchTest.java
+++ src/test/java/eavalyne/tests/eavalyneLt/SearchTest.java
@@ -1,4 +1,29 @@
 package eavalyne.tests.eavalyneLt;
 
-public class SearchTest {
+import eavalyne.pages.eavalyneLt.LoginPage;
+import eavalyne.pages.eavalyneLt.SearchPage;
+import eavalyne.tests.TestBase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SearchTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp(){
+        super.setUp();
+        LoginPage.open("https://eavalyne.lt/c/moterims");
+        LoginPage.acceptPrivacyConfirmation();
+    }
+
+    @Test
+    private void searchByItemBrandName(){
+        String insertItemName = "Adidas";
+        String expectedRezult = "";
+        String actualRezult = "";
+
+        SearchPage.enter
+
+
+    }
+
 }
