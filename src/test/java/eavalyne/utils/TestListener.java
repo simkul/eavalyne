1f2b73083894b26397b51a9ddc75c99fada1a4bc
simon <simonakuliesho@gmail.com>
2022-12-27 14:22:21 +0200

    Add TestListener

--- src/test/java/eavalyne/utils/TestListener.java
+++ src/test/java/eavalyne/utils/TestListener.java
@@ -1,4 +1,45 @@
 package eavalyne.utils;
 
-public class TestListener {
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+public class TestListener implements ITestListener {
+    public void onTestFailure(ITestResult result) {
+
+        takeScreenshot();
+    }
+
+    private void takeScreenshot() {
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        File screenshotRaw = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+
+        String directory = "./screenshots";
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss_SSS");
+        String dateTime = localDateTime.format(formatter);
+
+        File screenshotFile = new File(
+                directory + "/screenshot_" + dateTime + "_" + UUID.randomUUID() + ".png"
+        );
+
+        try {
+            FileUtils.copyFile(screenshotRaw, screenshotFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
